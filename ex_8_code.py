30b31e3c2516a9f9dce4d460b6f141f4ba74de4f
redperov <redperov@gmail.com>
2018-05-27 15:47:07 +0300

    Added data split

--- ex_8_code.py
+++ ex_8_code.py
@@ -2,47 +2,62 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.utils.data.sampler import SubsetRandomSampler
 from torchvision import datasets, transforms
+import numpy as np
 
 
 class FirstNet(nn.Module):
-
     def __init__(self, image_size):
         super(FirstNet, self).__init__()
         self.image_size = image_size
-        self.fc0 = nn.Linear(image_size, 1000)
-        self.fc1 = nn.Linear(1000, 50)
+        self.fc0 = nn.Linear(image_size, 100)
+        self.fc1 = nn.Linear(100, 50)
         self.fc2 = nn.Linear(50, 10)
 
     def forward(self, x):
         x = x.view(-1, self.image_size)
         x = F.relu(self.fc0(x))
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        return F.log_softmax(x)
+        return F.log_softmax(self.fc2(x), dim=1)
 
 
 def main():
+
+    # Load the data.
     train_loader, test_loader = load_data()
+
+    # Split the examples to training and validation sets 80:20.
+    train_loader, validation_loader = split_data(train_loader.dataset)
+
+    # Create the neural network.
     model = FirstNet(image_size=28 * 28)
 
+    # Initialize hyper-parameters.
     lr = 0.01
-    epochs = 30
+    epochs = 10
 
     optimizer = optim.SGD(model.parameters(), lr=lr)
 
     for epoch in range(epochs):
         train(epoch, model, optimizer, train_loader)
-        test(model, test_loader)
+        validate(epoch, model, validation_loader)
+
+    # Test the model.
+    test(model, test_loader)
 
 
 def load_data():
+    """
+    Loads the training and testing data.
+    :return: train_loader, test_loader
+    """
     t = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))])
 
     train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('./data', train=True, download=True,
+        datasets.FashionMNIST('./data', train=True, download=True,
                        transform=t),
         batch_size=64, shuffle=True)
 
@@ -51,6 +66,38 @@ def load_data():
         batch_size=64, shuffle=True)
 
     return train_loader, test_loader
+
+
+def split_data(data_set):
+    """
+    Splits the training data to training and validation 80:20.
+    :param data_set: data_set
+    :return: train_loader, validation_loader
+    """
+    num_train = len(data_set)
+    indices = list(range(num_train))
+    # split = 4
+    split = int(num_train * 0.2)
+
+    # Random, non-contiguous split
+    validation_idx = np.random.choice(indices, size=split, replace=False)
+    train_idx = list(set(indices) - set(validation_idx))
+
+    # Contiguous split
+    # train_idx, validation_idx = indices[split:], indices[:split]
+
+    ## define our samplers -- we use a SubsetRandomSampler because it will return
+    ## a random subset of the split defined by the given indices without replaf
+    train_sampler = SubsetRandomSampler(train_idx)
+    validation_sampler = SubsetRandomSampler(validation_idx)
+
+    train_loader = torch.utils.data.DataLoader(data_set,
+                    batch_size=1, sampler=train_sampler)
+
+    validation_loader = torch.utils.data.DataLoader(data_set,
+                    batch_size=1, sampler=validation_sampler)
+
+    return train_loader, validation_loader
 
 
 def train(epoch, model, optimizer, train_loader):
@@ -63,23 +110,37 @@ def train(epoch, model, optimizer, train_loader):
         optimizer.step()
 
 
+def validate(epoch, model, validation_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    for data, target in validation_loader:
+        output = model(data)
+        test_loss += F.nll_loss(output, target, size_average=False).item()  # sum up batch loss
+        pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+
+    test_loss /= len(validation_loader)
+    print('\nValidation set: Epoch: {} Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        epoch, test_loss, correct, len(validation_loader),
+        100.0 * correct / len(validation_loader)))
+
+
 def test(model, test_loader):
     model.eval()
     test_loss = 0
     correct = 0
     for data, target in test_loader:
         output = model(data)
-        test_loss += F.nll_loss(output, target, size_average=False).data[0] # sum up batch loss
-        pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
+        test_loss += F.nll_loss(output, target, size_average=False).item()  # sum up batch loss
+        pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
         correct += pred.eq(target.data.view_as(pred)).cpu().sum()
 
     test_loss /= len(test_loader.dataset)
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+        100.0 * correct / len(test_loader.dataset)))
 
 
 if __name__ == "__main__":
     main()
-
-
