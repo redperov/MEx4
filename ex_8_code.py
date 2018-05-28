e10853ed0be7151aa6e6436b57b5233f755eb8bb
redperov <redperov@gmail.com>
2018-05-28 22:50:57 +0300

    Before changes version

--- ex_8_code.py
+++ ex_8_code.py
@@ -8,9 +8,12 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-class FirstNet(nn.Module):
+class ModelA(nn.Module):
+    """
+    Neural network with two hidden layers.
+    """
     def __init__(self, image_size):
-        super(FirstNet, self).__init__()
+        super(ModelA, self).__init__()
         self.image_size = image_size
         self.fc0 = nn.Linear(image_size, 100)
         self.fc1 = nn.Linear(100, 50)
@@ -23,6 +26,50 @@ class FirstNet(nn.Module):
         return F.log_softmax(self.fc2(x), dim=1)
 
 
+class ModelB(nn.Module):
+    """
+    Neural network with two hidden layers and Dropout.
+    """
+    def __init__(self, image_size):
+        super(ModelB, self).__init__()
+        self.image_size = image_size
+        self.fc0 = nn.Linear(image_size, 100)
+        self.drop1 = nn.Dropout()
+        self.fc1 = nn.Linear(100, 50)
+        self.drop2 = nn.Dropout()
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = x.view(-1, self.image_size)
+        x = F.relu(self.fc0(x))
+        x = self.drop1(x)
+        x = F.relu(self.fc1(x))
+        x = self.drop2(x)
+        return F.log_softmax(self.fc2(x), dim=1)
+
+
+class ModelC(nn.Module):
+    """
+    Neural network with two hidden layers and Batch Normalization.
+    """
+    def __init__(self, image_size):
+        super(ModelC, self).__init__()
+        self.image_size = image_size
+        self.fc0 = nn.Linear(image_size, 100)
+        self.fc1 = nn.Linear(100, 50)
+        self.fc2 = nn.Linear(50, 10)
+        self.bn1 = nn.BatchNorm1d(100)
+        self.bn2 = nn.BatchNorm1d(50)
+
+    def forward(self, x):
+        x = x.view(-1, self.image_size)
+        x = self.bn1(x)
+        x = F.relu(self.fc0(x))
+        x = self.bn2(x)
+        x = F.relu(self.fc1(x))
+        return F.log_softmax(self.fc2(x), dim=1)
+
+
 def main():
 
     # Load the data.
@@ -31,21 +78,51 @@ def main():
     # Split the examples to training and validation sets 80:20.
     train_loader, validation_loader = split_data(train_loader.dataset)
 
-    # Create the neural network.
-    model = FirstNet(image_size=28 * 28)
+    # Initialize models.
+    model_a = ModelA(image_size=28 * 28)
+    model_b = ModelB(image_size=28 * 28)
+    model_c = ModelC(image_size=28 * 28)
 
     # Initialize hyper-parameters.
     lr = 0.01
     epochs = 10
 
-    optimizer = optim.SGD(model.parameters(), lr=lr)
+    # Setting the optimizers.
+    optimizer_a = optim.SGD(model_a.parameters(), lr=lr)
+    optimizer_b = optim.SGD(model_b.parameters(), lr=lr)
+    optimizer_c = optim.SGD(model_c.parameters(), lr=lr)
+
+    # Train and plot model A
+    # train_and_plot(model_a, optimizer_a, epochs, train_loader, validation_loader, test_loader)
+
+    # Train and plot model B
+    train_and_plot(model_b, optimizer_b, epochs, train_loader, validation_loader, test_loader)
+
+    # Train and plot model C
+    train_and_plot(model_c, optimizer_c, epochs, train_loader, validation_loader, test_loader)
+
+
+def train_and_plot(model, optimizer, epochs, train_loader, validation_loader, test_loader):
+    """
+    Trains and plots the results of a given model.
+    :param model: model
+    :param optimizer: optimizer
+    :param epochs: number of epochs
+    :param train_loader: train loader
+    :param validation_loader: validation loader
+    :param test_loader: test loader
+    :return: None
+    """
 
     train_loss_list = []
     validation_loss_list = []
 
     for epoch in range(epochs):
+        # Train the model.
         train_loss = train(epoch, model, optimizer, train_loader)
         train_loss_list.append(train_loss)
+
+        # Validate the model.
         validation_loss = validate(epoch, model, validation_loader)
         validation_loss_list.append(validation_loss)
 
@@ -53,6 +130,17 @@ def main():
     test(model, test_loader)
 
     # Plot average training and validation loss VS number of epochs
+    plot_graph(epochs, train_loss_list, validation_loss_list)
+
+
+def plot_graph(epochs, train_loss_list, validation_loss_list):
+    """
+    Plots a graph of training and validation average loss VS number of epochs.
+    :param epochs: number of epochs
+    :param train_loss_list: train loss list
+    :param validation_loss_list: validation loss list
+    :return: None
+    """
     epochs_list = list(range(epochs))
     plt.plot(epochs_list, train_loss_list, 'b', label="training loss")
     plt.plot(epochs_list, validation_loss_list, 'r--', label="validation loss")
@@ -91,18 +179,13 @@ def split_data(data_set):
     """
     num_train = len(data_set)
     indices = list(range(num_train))
-    # split = 4
     split = int(num_train * 0.2)
 
     # Random, non-contiguous split
     validation_idx = np.random.choice(indices, size=split, replace=False)
     train_idx = list(set(indices) - set(validation_idx))
 
-    # Contiguous split
-    # train_idx, validation_idx = indices[split:], indices[:split]
-
-    ## define our samplers -- we use a SubsetRandomSampler because it will return
-    ## a random subset of the split defined by the given indices without replaf
+    # Perform random split using subset random samples.
     train_sampler = SubsetRandomSampler(train_idx)
     validation_sampler = SubsetRandomSampler(validation_idx)
 
@@ -133,7 +216,7 @@ def train(epoch, model, optimizer, train_loader):
         loss = F.nll_loss(output, labels)
 
         # Calculation for checking the loss and accuracy.
-        training_loss += F.nll_loss(output, labels, size_average=False).item()  # sum up batch loss
+        training_loss += loss.item() #F.nll_loss(output, labels, size_average=False).item()  # sum up batch loss
         pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
         correct += pred.eq(labels.data.view_as(pred)).cpu().sum()
 
