6b254c0f002a10749b958a61fd944105cdbd358a
redperov <redperov@gmail.com>
2018-05-30 13:46:12 +0300

    Fixed model C

--- ex_8_code.py
+++ ex_8_code.py
@@ -15,7 +15,6 @@ class ModelA(nn.Module):
     def __init__(self, image_size):
         super(ModelA, self).__init__()
         self.image_size = image_size
-        # TODO how many layers there should be, 3 or 4?
         self.fc0 = nn.Linear(image_size, 100)
         self.fc1 = nn.Linear(100, 50)
         self.fc2 = nn.Linear(50, 10)
@@ -59,16 +58,14 @@ class ModelC(nn.Module):
         self.fc0 = nn.Linear(image_size, 100)
         self.fc1 = nn.Linear(100, 50)
         self.fc2 = nn.Linear(50, 10)
-        # TODO are the passed sizes correct?
-        self.bn0 = nn.BatchNorm1d(image_size)
-        self.bn1 = nn.BatchNorm1d(100)
+        self.bn0 = nn.BatchNorm1d(100)
+        self.bn1 = nn.BatchNorm1d(50)
 
     def forward(self, x):
         x = x.view(-1, self.image_size)
-        x = self.bn0(x)
-        x = F.relu(self.fc0(x))
+        x = F.relu(self.bn0(self.fc0(x)))
         x = self.bn1(x)
-        x = F.relu(self.fc1(x))
+        x = F.relu(self.bn1(self.fc1(x)))
         return F.log_softmax(self.fc2(x), dim=1)
 
 
@@ -86,7 +83,7 @@ def main():
     model_c = ModelC(image_size=28 * 28)
 
     # Initialize hyper-parameters.
-    lr = 0.05
+    lr = 0.01
     epochs = 10
 
     # Setting the optimizers.
@@ -95,13 +92,65 @@ def main():
     optimizer_c = optim.SGD(model_c.parameters(), lr=lr)
 
     # Train and plot model A
-    # train_and_plot(model_a, optimizer_a, epochs, train_loader, validation_loader, test_loader)
+    train_and_plot(model_a, optimizer_a, epochs, train_loader, validation_loader, test_loader)
 
     # Train and plot model B
     # train_and_plot(model_b, optimizer_b, epochs, train_loader, validation_loader, test_loader)
 
     # Train and plot model C
-    train_and_plot(model_c, optimizer_c, epochs, train_loader, validation_loader, test_loader)
+    # train_and_plot(model_c, optimizer_c, epochs, train_loader, validation_loader, test_loader)
+
+    # TODO write best model prediction
+    # write_prediction(model, test_loader)
+
+
+def load_data():
+    """
+    Loads the training and testing data.
+    :return: train_loader, test_loader
+    """
+    t = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))])
+
+    train_loader = torch.utils.data.DataLoader(
+        datasets.FashionMNIST('./data', train=True, download=True,
+                              transform=t),
+        batch_size=64, shuffle=False)
+
+    test_loader = torch.utils.data.DataLoader(
+        datasets.FashionMNIST('./data', train=False, download=True,
+                              transform=t),
+        batch_size=64, shuffle=False)
+
+    return train_loader, test_loader
+
+
+def split_data(data_set):
+    """
+    Splits the training data to training and validation 80:20.
+    :param data_set: data_set
+    :return: train_loader, validation_loader
+    """
+    num_train = len(data_set)
+    indices = list(range(num_train))
+    split = int(num_train * 0.2)
+
+    # Random, non-contiguous split
+    validation_idx = np.random.choice(indices, size=split, replace=False)
+    train_idx = list(set(indices) - set(validation_idx))
+
+    # Perform random split using subset random samples.
+    train_sampler = SubsetRandomSampler(train_idx)
+    validation_sampler = SubsetRandomSampler(validation_idx)
+
+    train_loader = torch.utils.data.DataLoader(data_set,
+                    batch_size=64, sampler=train_sampler)
+
+    validation_loader = torch.utils.data.DataLoader(data_set,
+                    batch_size=1, sampler=validation_sampler)
+
+    return train_loader, validation_loader
 
 
 def train_and_plot(model, optimizer, epochs, train_loader, validation_loader, test_loader):
@@ -155,54 +204,6 @@ def plot_graph(epochs, train_loss_list, validation_loss_list):
     plt.show()
 
 
-def load_data():
-    """
-    Loads the training and testing data.
-    :return: train_loader, test_loader
-    """
-    t = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,))])
-
-    train_loader = torch.utils.data.DataLoader(
-        datasets.FashionMNIST('./data', train=True, download=True,
-                       transform=t),
-        batch_size=64, shuffle=False)
-
-    test_loader = torch.utils.data.DataLoader(
-        datasets.FashionMNIST('./data', train=False, transform=t),
-        batch_size=64, shuffle=True)
-
-    return train_loader, test_loader
-
-
-def split_data(data_set):
-    """
-    Splits the training data to training and validation 80:20.
-    :param data_set: data_set
-    :return: train_loader, validation_loader
-    """
-    num_train = len(data_set)
-    indices = list(range(num_train))
-    split = int(num_train * 0.2)
-
-    # Random, non-contiguous split
-    validation_idx = np.random.choice(indices, size=split, replace=False)
-    train_idx = list(set(indices) - set(validation_idx))
-
-    # Perform random split using subset random samples.
-    train_sampler = SubsetRandomSampler(train_idx)
-    validation_sampler = SubsetRandomSampler(validation_idx)
-
-    train_loader = torch.utils.data.DataLoader(data_set,
-                    batch_size=64, sampler=train_sampler)
-
-    validation_loader = torch.utils.data.DataLoader(data_set,
-                    batch_size=64, sampler=validation_sampler)
-
-    return train_loader, validation_loader
-
-
 def train(model, optimizer, train_loader):
     """
     Trains the network.
@@ -231,11 +232,13 @@ def test(epoch, model, test_loader, set_type):
     model.eval()
     test_loss = 0
     correct = 0
+    counter = 0
     for data, target in test_loader:
         output = model(data)
         test_loss += F.nll_loss(output, target, size_average=False).item()  # sum up batch loss
         pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
         correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+        counter += 1
 
     test_loss /= len(test_loader.sampler)
     print('\n{}: Epoch: {} set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
@@ -243,6 +246,24 @@ def test(epoch, model, test_loader, set_type):
         100.0 * correct / len(test_loader.sampler)))
 
     return test_loss
+
+
+def write_prediction(model, test_loader):
+    """
+    Performs a prediction over the test set and writes it to a file.
+    :param model: model
+    :param test_loader: test loader
+    :return: None
+    """
+    with open("test.pred", "w") as test_file:
+        for data, target in test_loader:
+
+            # Pass the example through the classifier.
+            output = model(data)
+
+            # Extract the predicted label.
+            predicted_value = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
+            test_file.write(predicted_value + '\n')
 
 
 if __name__ == "__main__":
